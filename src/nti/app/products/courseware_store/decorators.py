d3b32b6eb850c90c3f15073939854195c8e688be
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-10-26 04:26:04 +0000

    add IsAvailable to the store enrollment option
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_store/trunk@52166 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_store/decorators.py
+++ src/nti/app/products/courseware_store/decorators.py
@@ -11,15 +11,23 @@ logger = __import__('logging').getLogger(__name__)
 from zope import component
 from zope import interface
 
+from nti.app.products.courseware.utils import get_enrollment_record
+
 from nti.app.renderers.decorators import AbstractAuthenticatedRequestAwareDecorator
 
+from nti.contenttypes.courses.interfaces import ES_PUBLIC
+from nti.contenttypes.courses.interfaces import ICourseCatalog 
+from nti.contenttypes.courses.interfaces import ICourseInstance
 from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
 
 from nti.externalization.externalization import to_external_object
+from nti.externalization.interfaces import IExternalObjectDecorator
 from nti.externalization.interfaces import IExternalMappingDecorator
 	
 from nti.store.purchasable import get_purchasable
 from nti.store.interfaces import IPurchasableCourse
+
+from .interfaces import IStoreEnrollmentOption
 
 @component.adapter(ICourseCatalogEntry)
 @interface.implementer(IExternalMappingDecorator)
@@ -41,3 +49,33 @@ class _StoreCourseEntryLinkDecorator(AbstractAuthenticatedRequestAwareDecorator)
 			store_enrollment['Price'] = ext_obj.get('Amount', None)
 			store_enrollment['Currency'] = ext_obj.get('Currency', None)
 			store_enrollment['IsEnrolled'] = ext_obj.get('Activated', False)
+
+@component.adapter(IStoreEnrollmentOption)
+@interface.implementer(IExternalObjectDecorator)
+class _StoreEnrollmentOptionLinkDecorator(AbstractAuthenticatedRequestAwareDecorator):
+
+	def _predicate(self, context, result):
+		return self._is_authenticated
+	
+	@classmethod
+	def _get_course(cls, context):
+		try:
+			catalog = component.getUtility(ICourseCatalog)
+			entry = catalog.getCatalogEntry(context.CatalogEntryNTIID)
+			course = ICourseInstance(entry, None)
+			return course
+		except (KeyError, AttributeError):
+			pass
+		return None
+	
+	@classmethod
+	def _get_enrollment_record(cls, context, remoteUser):
+		course = cls._get_course(context)
+		if course is not None:
+			return get_enrollment_record(course, remoteUser)
+		return None
+	
+	def _do_decorate_external(self, context, result):
+		record = self._get_enrollment_record(context, self.remoteUser)
+		isAvailable = bool(record is None or record.Scope == ES_PUBLIC)
+		result['IsAvailable'] = isAvailable
