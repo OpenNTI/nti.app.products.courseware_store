2dc1538bec7faa28077b75f5e7326b8908b69c48
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-11-11 16:21:03 +0000

    remove extra decorator that was giving wrong info for enrollment option. One more thing to fix
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_store/trunk@53397 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_store/decorators.py
+++ src/nti/app/products/courseware_store/decorators.py
@@ -19,41 +19,14 @@ from nti.contenttypes.courses.interfaces import ES_PUBLIC
 from nti.contenttypes.courses.interfaces import ES_PURCHASED
 from nti.contenttypes.courses.interfaces import ICourseCatalog 
 from nti.contenttypes.courses.interfaces import ICourseInstance
-from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
 
-from nti.externalization.externalization import to_external_object
 from nti.externalization.interfaces import IExternalObjectDecorator
-from nti.externalization.interfaces import IExternalMappingDecorator
-	
-from nti.store.purchasable import get_purchasable
-from nti.store.interfaces import IPurchasableCourse
 
 from .interfaces import IStoreEnrollmentOption
 
-@component.adapter(ICourseCatalogEntry)
-@interface.implementer(IExternalMappingDecorator)
-class _StoreCourseEntryLinkDecorator(AbstractAuthenticatedRequestAwareDecorator):
-
-	def _predicate(self, context, result):
-		return self._is_authenticated
-	
-	def _do_decorate_external(self, context, result):
-		purchasable = IPurchasableCourse(context, None)
-		purchasable = get_purchasable(purchasable.NTIID) if purchasable else None
-		if purchasable is not None and purchasable.Public:
-			options = result.setdefault('EnrollmentOptions', {})
-			store_enrollment = options.setdefault('StoreEnrollment', {})
-			# set purchasable
-			ext_obj = to_external_object(purchasable, name='summary')
-			store_enrollment['Purchasable'] = ext_obj
-			store_enrollment['RequiresAdmission'] = False
-			store_enrollment['Price'] = ext_obj.get('Amount', None)
-			store_enrollment['Currency'] = ext_obj.get('Currency', None)
-			store_enrollment['IsEnrolled'] = ext_obj.get('Activated', False)
-
 @component.adapter(IStoreEnrollmentOption)
 @interface.implementer(IExternalObjectDecorator)
-class _StoreEnrollmentOptionLinkDecorator(AbstractAuthenticatedRequestAwareDecorator):
+class _StoreEnrollmentOptionDecorator(AbstractAuthenticatedRequestAwareDecorator):
 
 	def _predicate(self, context, result):
 		return self._is_authenticated
@@ -79,6 +52,6 @@ class _StoreEnrollmentOptionLinkDecorator(AbstractAuthenticatedRequestAwareDecor
 	def _do_decorate_external(self, context, result):
 		record = self._get_enrollment_record(context, self.remoteUser)
 		isAvailable = bool(record is None or record.Scope == ES_PUBLIC)
-		result['IsAvailable'] = isAvailable
+		result['Enabled'] = result['IsAvailable'] = isAvailable
 		IsEnrolled = record is not None and record.Scope == ES_PURCHASED
 		result['IsEnrolled'] = IsEnrolled
