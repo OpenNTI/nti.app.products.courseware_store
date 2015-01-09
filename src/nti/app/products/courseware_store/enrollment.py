4eee3d22fd6fc21ffb30254ced91a8e3cd0539d0
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-01-09 05:32:36 +0000

    return enrollment options for the parent course the current course is a sub instance
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_store/trunk@56946 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_store/enrollment.py
+++ src/nti/app/products/courseware_store/enrollment.py
@@ -15,6 +15,8 @@ from zope.schema.fieldproperty import FieldPropertyStoredThroughField as FP
 from nti.app.products.courseware.enrollment import EnrollmentOption
 from nti.app.products.courseware.interfaces import IEnrollmentOptionProvider
 
+from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import ICourseSubInstance
 from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
 
 from nti.externalization.persistence import NoPickle
@@ -65,14 +67,32 @@ class StoreEnrollmentOptionProvider(object):
 	def __init__(self, context):
 		self.context = context
 		
-	def iter_options(self):
-		provider = get_entry_purchasable_provider(self.context)
-		ntiid = get_entry_purchasable_ntiid(self.context, provider)
+	def get_purchasable(self, context):
+		provider = get_entry_purchasable_provider(context)
+		ntiid = get_entry_purchasable_ntiid(context, provider)
 		purchasable = get_purchasable(ntiid) if ntiid else None
+		return purchasable
+	
+	def get_context(self):
+		course = ICourseInstance(self.context)
+		purchasable = self.get_purchasable(self.context)
+		## CS: if we cannot get a purchasable and the context course is a 
+		## sub-instance try with its parent course. This may happen
+		## with mapped courses
+		if 	(purchasable is None or not purchasable.Public) and \
+			ICourseSubInstance.providedBy(course):
+			result = ICourseCatalogEntry(course.__parent__.__parent__)
+		else:
+			result = self.context
+		return result
+		
+	def iter_options(self):
+		context = self.get_context()
+		purchasable = self.get_purchasable(context)
 		if purchasable is not None and purchasable.Public:
 			result = StoreEnrollmentOption()
 			result.Purchasable = purchasable
-			result.CatalogEntryNTIID = self.context.ntiid
-			result.AllowVendorUpdates = allow_vendor_updates(self.context)
+			result.CatalogEntryNTIID = context.ntiid
+			result.AllowVendorUpdates = allow_vendor_updates(context)
 			return (result,)
 		return ()
