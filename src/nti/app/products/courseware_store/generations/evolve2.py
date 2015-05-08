b0b44f59f5f171ea91bc17f8b8f8cf277ea43c02
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-05-08 02:24:16 +0000

    stub out generations
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_store/trunk@64747 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/products/courseware_store/generations/evolve2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+generation = 2
+
+def do_evolve(context):
+	pass
+
+def evolve(context):
+	"""
+	Evolve to generation 2 by registering purchasables for courses
+	"""
+	do_evolve(context)
+
--- src/nti/app/products/courseware_store/tests/__init__.py
+++ src/nti/app/products/courseware_store/tests/__init__.py
@@ -33,5 +33,36 @@ class CourseStoreApplicationTestLayer(InstructedCourseApplicationTestLayer):
     def testTearDown(cls):
         pass
 
-class OUCoursewareApplicationLayerTest(ApplicationLayerTest):
-    layer = CourseStoreApplicationTestLayer
+from nti.testing.layers import GCLayerMixin
+from nti.testing.layers import ZopeComponentLayer
+from nti.testing.layers import ConfiguringLayerMixin
+
+from nti.dataserver.tests.mock_dataserver import DSInjectorMixin
+
+import zope.testing.cleanup
+
+class SharedConfiguringTestLayer(ZopeComponentLayer,
+                                 GCLayerMixin,
+                                 ConfiguringLayerMixin,
+                                 DSInjectorMixin):
+
+    set_up_packages = ('nti.dataserver',
+                       'nti.app.client_preferences',
+                       'nti.app.products.courseware_store')
+
+    @classmethod
+    def setUp(cls):
+        cls.setUpPackages()
+
+    @classmethod
+    def tearDown(cls):
+        cls.tearDownPackages()
+        zope.testing.cleanup.cleanUp()
+
+    @classmethod
+    def testSetUp(cls, test=None):
+        cls.setUpTestDS(test)
+
+    @classmethod
+    def testTearDown(cls):
+        pass
