da98e996efff6ccb5ee4881f122f6edf3d371a60
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-11-06 03:11:50 +0000

    update code
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_store/trunk@76408 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_store/generations/evolve3.py
+++ src/nti/app/products/courseware_store/generations/evolve3.py
@@ -9,33 +9,36 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from zope import component
+
 from zope.component.hooks import setHooks
 from zope.component.hooks import site as current_site
 
-from zope.traversing.interfaces import IEtcNamespace
-
-from nti.site.interfaces import IHostPolicyFolder
+from nti.site.hostpolicy import get_all_host_sites
 
 from ..register import register_site_purchasables
 
 generation = 2
 
-def do_evolve(context):
-	setHooks()
+def register():
 	seen = set()
-	conn = context.connection
-	ds_folder = conn.root()['nti.dataserver']
-	lsm = ds_folder.getSiteManager()		
-	sites_folder = lsm.getUtility(IEtcNamespace, name='hostsites')
-	for _, site in sites_folder.items():
-		if not IHostPolicyFolder.providedBy(site):
-			continue	
+	for site in get_all_host_sites():
 		with current_site(site):
 			registry = site.getSiteManager()
 			register_site_purchasables(registry=registry, seen=seen)
 
+def do_evolve(context, generation=generation):
+	setHooks()
+	conn = context.connection
+	ds_folder = conn.root()['nti.dataserver']
+	with current_site(ds_folder):
+		assert	component.getSiteManager() == ds_folder.getSiteManager(), \
+				"Hooks not installed?"
+		register()
+	logger.info('nti.dataserver-courseware-store %s generation completed', generation)
+
 def evolve(context):
 	"""
-	Evolve to generation 2 by registering purchasables for courses
+	Evolve to generation 3 by registering purchasables for courses
 	"""
 	do_evolve(context)
