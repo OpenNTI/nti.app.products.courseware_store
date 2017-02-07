623b5031f6c5be9d4a7c1feaafaae7765a8a5268
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-07 03:20:00 +0000

    whitespace
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_store/trunk@105962 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -7,36 +7,36 @@ entry_points = {
     'console_scripts': [
     ],
     "z3c.autoinclude.plugin": [
-		'target = nti.app.products',
-	],
+        'target = nti.app.products',
+    ],
 }
 
 setup(
-    name = 'nti.app.products.courseware_store',
-    version = VERSION,
-    author = 'Carlos Sanchez',
-    author_email = 'carlos@nextthought.com',
-    description = "Course-Store Integration",
-    long_description = codecs.open('README.rst', encoding='utf-8').read(),
-    license = 'Proprietary',
-    keywords = 'Course Store',
-    classifiers = [
+    name='nti.app.products.courseware_store',
+    version=VERSION,
+    author='Carlos Sanchez',
+    author_email='carlos@nextthought.com',
+    description="Course-Store Integration",
+    long_description=codecs.open('README.rst', encoding='utf-8').read(),
+    license='Proprietary',
+    keywords='Course Store',
+    classifiers=[
         'Framework :: Pyramid',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-		'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: Implementation :: CPython'
     ],
-	packages=find_packages('src'),
-	package_dir={'': 'src'},
-	namespace_packages=['nti', 'nti.app', 'nti.app.products'],
-	install_requires=[
-		'setuptools',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['nti', 'nti.app', 'nti.app.products'],
+    install_requires=[
+        'setuptools',
         'nti.store',
         'nti.app.store',
         'nti.app.products.courseware'
-	],
-	entry_points=entry_points
+    ],
+    entry_points=entry_points
 )
