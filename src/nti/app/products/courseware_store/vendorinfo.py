b020fa8aa3d2c31c906c6a29a2014de71f7d7cc3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-13 02:06:14 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_store/trunk@112644 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_store/vendorinfo.py
+++ src/nti/app/products/courseware_store/vendorinfo.py
@@ -4,7 +4,7 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -32,15 +32,13 @@ class _CourseCatalogPublishableVendorInfo(object):
         does_allow_vendor_updates = allow_vendor_updates(self.context)
         result = {
             'Title': catalog_entry.title,
-            'StartDate': catalog_entry.StartDate,
             'EndDate': catalog_entry.EndDate,
             'Duration': catalog_entry.Duration,
+            'StartDate': catalog_entry.StartDate,
             'AllowVendorUpdates': does_allow_vendor_updates
         }
         credit_info = getattr(catalog_entry, 'Credit', None)
         if credit_info:
-            # Just get the first entry
-            credit_info = credit_info[0]
-            hours = credit_info.Hours
-            result.update({'Hours': hours})
+            credit_info = credit_info[0] # get the first entry
+            result.update({'Hours': credit_info.Hours})
         return result
