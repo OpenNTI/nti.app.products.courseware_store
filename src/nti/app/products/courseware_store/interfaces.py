82397ca2505e77f56b3a076904eb3199bf375b12
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-06-06 01:17:47 +0000

    ensure unicode
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_store/trunk@114317 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_store/interfaces.py
+++ src/nti/app/products/courseware_store/interfaces.py
@@ -4,7 +4,7 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -33,10 +33,10 @@ get_course_publishable_vendor_info = get_course_publishable_vendor_info
 
 
 class ICoursePrice(interface.Interface):
-    Amount = Number(title="The price amount", required=True)
-    Currency = ValidTextLine(title="The currency",
+    Amount = Number(title=u"The price amount", required=True)
+    Currency = ValidTextLine(title=u"The currency",
                              required=False,
-                             default='USD')
+                             default=u'USD')
 
 
 class ICoursePriceFinder(interface.Interface):
@@ -46,29 +46,29 @@ class ICoursePriceFinder(interface.Interface):
 
 
 class IStoreEnrollmentOption(IEnrollmentOption):
-    IsEnabled = Bool(title="Is enabled flag",
+    IsEnabled = Bool(title=u"Is enabled flag",
                      required=False,
                      default=True)
 
     Purchasables = ListOrTuple(Object(IPurchasableCourse),
-                               title="Purchasable course",
+                               title=u"Purchasable course",
                                required=True,
                                min_length=1)
 
-    AllowVendorUpdates = Bool(title="Allow vendor updates",
+    AllowVendorUpdates = Bool(title=u"Allow vendor updates",
                               required=False,
                               default=False)
 
 
 class IStoreEnrollmentEvent(interface.Interface):
-    request = Object(IRequest, title="the request", required=False)
+    request = Object(IRequest, title=u"the request", required=False)
 
     purchasable = Object(IPurchasableCourse,
-                         title="purchasable course",
+                         title=u"purchasable course",
                          required=False)
 
     record = Object(ICourseInstanceEnrollmentRecord,
-                    title="enrollemnt record",
+                    title=u"enrollemnt record",
                     required=True)
 
 
