02ab48f82667e5664faab108da609da0eaba99eb
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-07-13 21:19:43 +0000

    start moving course purchasable objects
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_store/trunk@117380 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_store/interfaces.py
+++ src/nti/app/products/courseware_store/interfaces.py
@@ -21,11 +21,20 @@ from nti.app.products.courseware.interfaces import IEnrollmentOption
 from nti.contenttypes.courses.interfaces import ICourseInstanceEnrollmentRecord
 
 from nti.store.interfaces import IPrice
+from nti.store.interfaces import IPurchasable
 from nti.store.interfaces import IPurchasableCourse
+from nti.store.interfaces import IPurchasableVendorInfo
+from nti.store.interfaces import IPurchasableChoiceBundle
 
 from nti.schema.field import Bool
+from nti.schema.field import Number
 from nti.schema.field import Object
+from nti.schema.field import Datetime
+from nti.schema.field import Timedelta
+from nti.schema.field import ValidText
 from nti.schema.field import ListOrTuple
+from nti.schema.field import ValidTextLine
+from nti.schema.field import UniqueIterable
 
 
 class ICoursePrice(IPrice):
@@ -36,6 +45,61 @@ class ICoursePriceFinder(interface.Interface):
     """
     marker interface for a course price finder
     """
+
+
+class IPurchasableCourse2(IPurchasable):
+
+    Name = ValidTextLine(title=u'Course Name', 
+                         required=False)
+
+    # overrides
+    Amount = Number(title=u"Cost amount",
+                    required=False,
+                    min=0.0,
+                    default=0.0)
+
+    Provider = ValidTextLine(title=u'Course provider',
+                             required=False)
+
+    # Deprecated/Legacy fields
+    Featured = Bool(title=u'Featured flag',
+                    required=False,
+                    default=False)
+
+    Preview = Bool(title=u'Course preview flag',
+                   required=False)
+
+    StartDate = ValidTextLine(title=u"Course start date",
+                              required=False)
+
+    Department = ValidTextLine(title=u'Course Department',
+                               required=False)
+
+    Signature = ValidText(title=u'Course/Professor Signature',
+                          required=False)
+
+    Communities = UniqueIterable(value_type=ValidTextLine(title=u'The community identifier'),
+                                 title=u"Communities", 
+                                 required=False)
+
+    Duration = Timedelta(title=u"The length of the course",
+                         description=u"Currently optional, may be None",
+                         required=False)
+
+    EndDate = Datetime(title=u"The date on which the course ends",
+                       required=False)
+
+    # For purchaseables, we want to share this.
+    VendorInfo = Object(IPurchasableVendorInfo,
+                        title=u"vendor info", 
+                        required=False)
+    VendorInfo.setTaggedValue('_ext_excluded_out', False)
+ICourse = IPurchasableCourse  # alias BWC
+
+
+class IPurchasableCourseChoiceBundle(IPurchasableChoiceBundle,
+                                     IPurchasableCourse):
+    pass
 
 
 class IStoreEnrollmentOption(IEnrollmentOption):
--- src/nti/app/products/courseware_store/model.py
+++ src/nti/app/products/courseware_store/model.py
@@ -11,9 +11,20 @@ logger = __import__('logging').getLogger(__name__)
 
 from zope import interface
 
-from nti.app.products.courseware_store.interfaces import ICoursePrice
+from zope.cachedescriptors.property import readproperty
 
+from nti.app.products.courseware_store.interfaces import ICoursePrice
+from nti.app.products.courseware_store.interfaces import IPurchasableCourse2
+from nti.app.products.courseware_store.interfaces import IPurchasableCourseChoiceBundle
+
+from nti.externalization.representation import WithRepr
+
+from nti.schema.eqhash import EqHash
+
+from nti.schema.fieldproperty import AdaptingFieldProperty
 from nti.schema.fieldproperty import createDirectFieldProperties
+
+from nti.store.purchasable import Purchasable
 
 from nti.store.model import Price
 
@@ -21,3 +32,22 @@ from nti.store.model import Price
 @interface.implementer(ICoursePrice)
 class CoursePrice(Price):
     createDirectFieldProperties(ICoursePrice)
+
+
+@WithRepr
+@EqHash('NTIID',)
+@interface.implementer(IPurchasableCourse2)
+class PurchasableCourse(Purchasable):
+    createDirectFieldProperties(IPurchasableCourse2)
+
+    Description = AdaptingFieldProperty(IPurchasableCourse2['Description'])
+
+    @readproperty
+    def Label(self):
+        return self.Name
+
+
+@interface.implementer(IPurchasableCourseChoiceBundle)
+class PurchasableCourseChoiceBundle(PurchasableCourse):
+    __external_class_name__ = 'PurchasableCourseChoiceBundle'
+    IsPurchasable = False
