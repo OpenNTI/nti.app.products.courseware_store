d48dca1fe64b463b5ee51d6cc82d4f2c100bcf06
Josh Zuech <josh.zuech@nextthought.com>
2020-04-16 15:40:22 -0500

    Quick purchasable api

--- src/nti/app/products/courseware_store/views.py
+++ src/nti/app/products/courseware_store/views.py
@@ -10,9 +10,14 @@ from __future__ import absolute_import
 
 import csv
 import six
+
 from io import BytesIO
 
 from requests.structures import CaseInsensitiveDict
+
+from zope import lifecycleevent
+
+from zope.cachedescriptors.property import Lazy
 
 from zope.component.hooks import site as current_site
 
@@ -23,17 +28,34 @@ from pyramid.view import view_defaults
 
 from nti.app.base.abstract_views import AbstractAuthenticatedView
 
+from nti.app.externalization.error import raise_json_error
+
+from nti.app.externalization.view_mixins import ModeledContentUploadRequestUtilsMixin
+
 from nti.app.products.courseware.views import CourseAdminPathAdapter
 
 from nti.app.products.courseware_store import MessageFactory as _
+
+from nti.app.products.courseware_store.interfaces import IPurchasableCourse
+
+from nti.app.products.courseware_store.model import create_course
 
 from nti.app.products.courseware_store.purchasable import get_registered_choice_bundles
 from nti.app.products.courseware_store.purchasable import sync_purchasable_course_choice_bundles
 
 from nti.app.products.courseware_store.utils import find_catalog_entry
+from nti.app.products.courseware_store.utils import has_store_connect_keys
+from nti.app.products.courseware_store.utils import can_edit_course_purchasable
+from nti.app.products.courseware_store.utils import get_course_purchasable_ntiid
 from nti.app.products.courseware_store.utils import find_allow_vendor_updates_purchases
+from nti.app.products.courseware_store.utils import can_course_have_editable_purchasable
+
+from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
 
 from nti.dataserver import authorization as nauth
+
+from nti.dataserver.authorization import is_admin
 
 from nti.externalization.interfaces import LocatedExternalDict
 from nti.externalization.interfaces import StandardExternalFields
@@ -41,6 +63,8 @@ from nti.externalization.interfaces import StandardExternalFields
 from nti.site.hostpolicy import get_host_site
 
 from nti.site.site import get_component_hierarchy_names
+
+from nti.store.store import register_purchasable
 
 ITEMS = StandardExternalFields.ITEMS
 TOTAL = StandardExternalFields.TOTAL
@@ -117,3 +141,100 @@ class SyncPurchasableCourseChoiceBundlesView(AbstractAuthenticatedView):
         items = result[ITEMS] = list(bundles.values())
         result[TOTAL] = result[ITEM_COUNT] = len(items)
         return result
+
+
+@view_config(context=ICourseInstance)
+@view_config(context=ICourseCatalogEntry)
+@view_defaults(route_name='objects.generic.traversal',
+               renderer='rest',
+               name='CreateCoursePurchasable')
+class CreateCoursePurchasableView(AbstractAuthenticatedView,
+                                  ModeledContentUploadRequestUtilsMixin):
+    """
+    Creates or updates a course purchasable
+    """
+
+    DEFAULT_FEE = None
+    DEFAULT_CURRENCY = u'USD'
+    DEFAULT_STRIPE_PROVIDER = u'default'
+
+    VALID_FIELDS = ('Currency', 'Provider', 'Amount')
+
+    @Lazy
+    def _course(self):
+        return ICourseInstance(self.context)
+
+    @Lazy
+    def _catalog_entry(self):
+        return ICourseCatalogEntry(self.context)
+
+    def readInput(self, value=None):
+        result = super(CreateCoursePurchasableView, self).readInput(value)
+        result = CaseInsensitiveDict(result)
+        # Sanitize; no fee edits unless NT admin
+        valid_fields = self.VALID_FIELDS
+        if is_admin(self.remoteUser):
+            valid_fields = valid_fields + ('Fee',)
+        for key in tuple(result):
+            if key not in valid_fields:
+                result.pop(key, None)
+        return result
+
+    def create_purchasable(self):
+        # TODO: utility for default fees/currency
+        entry = self._catalog_entry
+        ntiid = get_course_purchasable_ntiid(entry)
+        result = create_course(ntiid=ntiid,
+                               items=(entry.ntiid,),
+                               name=entry.title,
+                               title=entry.title,
+                               provider=self.DEFAULT_STRIPE_PROVIDER,
+                               fee=self.DEFAULT_FEE,
+                               currency=self.DEFAULT_CURRENCY,
+                               description=entry.description)
+        return result
+
+    def validate(self):
+        purchasable = IPurchasableCourse(self._entry, None)
+        if purchasable is not None:
+            raise_json_error(self.request,
+                             hexc.HTTPUnprocessableEntity,
+                             {
+                                'message': _(u"This course is already purchasable"),
+                                'field': 'CourseAlreadyPurchasableError'
+                             },
+                             None)
+        if not has_store_connect_keys():
+            raise_json_error(self.request,
+                             hexc.HTTPUnprocessableEntity,
+                             {
+                                 'message': _(u'Cannot make purchasable without connect key.'),
+                             },
+                             None)
+        if not can_edit_course_purchasable(self._course, self.remoteUser):
+            raise_json_error(self.request,
+                             hexc.HTTPForbidden,
+                             {
+                                 'message': _(u'Not an admin or instructor.'),
+                             },
+                             None)
+        if not can_course_have_editable_purchasable(self._course):
+            raise_json_error(self.request,
+                             hexc.HTTPUnprocessableEntity,
+                             {
+                                'message': _(u"This course is already purchasable"),
+                                'field': 'CourseAlreadyPurchasableError'
+                             },
+                             None)
+
+    def __call__(self):
+        # TODO: License restrictions
+        self.validate()
+        externalValue = self.readInput()
+        result = self.create_purchasable()
+        self.updateContentObject(result, externalValue)
+        lifecycleevent.created(result)
+        register_purchasable(result)
+        result.__parent__ = self._course
+        return result
+
